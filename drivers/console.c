db750c974c4d6ef180cbe07687cda5a28b686021
AceVest <zhaoyanbai@126.com>
2014-07-20 12:00:44 +0800

    support read from console

--- drivers/console.c
+++ drivers/console.c
@@ -9,27 +9,161 @@
 
 #include<string.h>
 #include<console.h>
+#include<wait.h>
 
-cnsl_queue_t cnsl_rd_q;
-cnsl_queue_t cnsl_wr_q;
-cnsl_queue_t cnsl_sc_q;
+cnsl_t cnsl;
+
+static bool empty(const cnsl_queue_t *q)
+{
+    return q->head == q->tail;
+}
+
+static bool full(const cnsl_queue_t *q)
+{
+    return (q->head + 1) % CNSL_QUEUE_SIZE == q->tail;
+}
+
+static void put(cnsl_queue_t *q, char c)
+{
+    if(!full(q))
+    {
+        q->data[q->head] = c;
+        q->head = (q->head + 1) % CNSL_QUEUE_SIZE;
+    }
+}
+
+static bool get(cnsl_queue_t *q, char *c)
+{
+    if(!empty(q))
+    {
+        *c = q->data[q->tail];
+        q->tail = (q->tail + 1) % CNSL_QUEUE_SIZE;
+        return true;
+    }
+
+    return false;
+}
+
+static void clear(cnsl_queue_t *q)
+{
+    q->head = q->tail = 0;
+}
+
+static void erase(cnsl_queue_t *q)
+{
+    if(empty(q))
+        return;
+
+    if(q->head == 0)
+        q->head = CNSL_QUEUE_SIZE -1;
+    else
+        q->head--;
+}
+
 
 static void cnsl_queue_init(cnsl_queue_t *cq)
 {
     memset((void *)cq, 0, sizeof(*cq));
-
     cq->head = 0;
     cq->tail = 0;
     init_wait_queue(&cq->wait);
-
-    //cq->data = kmalloc(CNSL_QUEUE_SIZE, 0);
-
-    printk("console queue data addr %08x\n", cq->data);
 }
+
+wait_queue_head_t rdwq;
 
 void cnsl_init()
 {
-    cnsl_queue_init(&cnsl_rd_q);
-    cnsl_queue_init(&cnsl_wr_q);
-    cnsl_queue_init(&cnsl_sc_q);
+    cnsl_queue_init(&cnsl.rd_q);
+    cnsl_queue_init(&cnsl.wr_q);
+    cnsl_queue_init(&cnsl.sc_q);
+    init_wait_queue(&rdwq);
 }
+
+
+int cnsl_kbd_write(char ch)
+{
+    if(ch == '\b')
+    {
+        if(!empty(&cnsl.wr_q))
+            vga_putc(0, '\b', 0x2);
+        erase(&cnsl.wr_q);
+        erase(&cnsl.sc_q);
+    }
+    else
+    {
+        put(&cnsl.wr_q, ch);
+        put(&cnsl.sc_q, ch);
+        vga_putc(0, ch, 0x2);
+    }
+
+
+    if(ch == '\n')
+    {
+        clear(&cnsl.wr_q);
+        while(get(&cnsl.sc_q, &ch))
+            put(&cnsl.rd_q, ch);
+        wake_up(&rdwq);
+    }
+
+#if 0
+    printl(23, "rd: %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x",
+    cnsl.rd_q.data[0],
+    cnsl.rd_q.data[1],
+    cnsl.rd_q.data[2],
+    cnsl.rd_q.data[3],
+    cnsl.rd_q.data[4],
+    cnsl.rd_q.data[5],
+    cnsl.rd_q.data[6],
+    cnsl.rd_q.data[7],
+    cnsl.rd_q.data[8],
+    cnsl.rd_q.data[9]);
+#endif
+}
+
+
+int cnsl_read(char *buf, size_t count)
+{
+    unsigned long flags;
+
+    assert(count > 0);
+    int cnt = 0;
+    for(cnt=0; cnt<count; )
+    {
+        char ch;
+        task_union * task = current;
+        DECLARE_WAIT_QUEUE(wait, task);
+        add_wait_queue(&rdwq, &wait);
+
+        while(true)
+        {
+            task->state = TASK_WAIT;
+            irq_save(flags);
+            bool r = get(&cnsl.rd_q, &ch);
+            irq_restore(flags);
+
+            if(r)
+            {
+                buf[cnt++] = ch;
+
+                task->state = TASK_RUNNING;
+                del_wait_queue(&rdwq, &wait);
+
+                if(ch == '\n')
+                    goto end;
+
+                break;
+            }
+
+            schedule();
+        }
+
+    }
+
+end:
+    buf[cnt] = 0;
+    return cnt;
+}
+
+chrdev_t cnsl_chrdev = {
+    .read = cnsl_read
+};
--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -29,37 +29,56 @@ void vga_dbg_toggle();
 int debug_wait_queue_put(unsigned int v);
 void ide_dma_pci_lba48();
 
-unsigned long kbd_cnt = 0;
+
+void kbd_debug(unsigned char scan_code);
+
+char kbd_char_tbl[]={0,0,
+'1','2','3','4','5','6','7','8','9','0','-','=','\b',0,
+'q','w','e','r','t','y','u','i','o','p','[',']','\n',0,
+'a','s','d','f','g','h','j','k','l',';','\'','`',0,'\\',
+'z','x','c','v','b','n','m',',','.','/',0,0,0,' ',
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+};
+
 
 void kbd_handler(unsigned int irq, pt_regs_t * regs, void *dev_id)
 {
     unsigned char scan_code;
+
     scan_code = inb(0x60);
 
+    kbd_debug(scan_code);
+
+    if(0x80 & scan_code)   // break code
+    {
+        return ;
+    }
+
+    unsigned int inx = scan_code & 0xFF;
+    char ch = kbd_char_tbl[inx];
+    cnsl_kbd_write(ch);
+}
+
+void kbd_debug(unsigned char scan_code)
+{
+    static unsigned long kbd_cnt = 0;
     printl(MPL_KEYBOARD, "keyboard:%d scan code %02x", kbd_cnt++, scan_code);
 
     if(scan_code == 0x01) // Esc
         reboot();
-    
-    printk("[%02x]", scan_code);
 
-    if(scan_code == 0x09)   // 8
-        ide_dma_pci_lba48();
+    printd("[%02x]", scan_code);
 
-    if(scan_code == 0x0B)   // 0
-    {
-        asm("cli;");
-        while(1);
-    }
-
-    if(scan_code == 0x13)   // r
-        ide_debug();
-
-    if(scan_code == 0x1F)   // s
-        ide_status();
-
-    if(scan_code == 0x14)   // t
-        debug_sched();
 
     if(scan_code == 0x3B)   // F1
         vga_switch(0);
@@ -79,22 +98,18 @@ void kbd_handler(unsigned int irq, pt_regs_t * regs, void *dev_id)
     if(scan_code == 0x42)   // F8
         debug_wait_queue_put(7);
 
-    if(scan_code == 0x43);  // F9
-    if(scan_code == 0x44);  // F10
-    if(scan_code == 0x57);  // F11
+    if(scan_code == 0x43)   // F9
+        ide_dma_pci_lba48();
+    if(scan_code == 0x44)   // F10
+        ide_debug();
+    if(scan_code == 0x57)   // F11
+    {
+        asm("cli;");
+        while(1);
+    }
     if(scan_code == 0x58)   // F12
         vga_dbg_toggle();
 
-#if 1
-    cnsl_rd_q.data[0] = (char) scan_code;
-    wake_up(&cnsl_rd_q.wait);
-#endif
-}
 
-int sysc_read_kbd()
-{
-    DECLARE_WAIT_QUEUE(wait, current);
-    add_wait_queue(&cnsl_rd_q.wait, &wait);
-
-    return 0;
+    //ide_status();
 }
