6640eabe45e2675567034ab0f22e7c37d23bd0ce
acevest <zhaoyanbai@126.com>
2021-10-30 23:57:11 +0800

    add docker copy shell script and use new .bochsrc

---
+++ scripts/docker_copy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+kpartx_out=`kpartx -l HD.IMG`
+PART=`echo ${kpartx_out}|awk '{print $1}'`
+PART=/dev/mapper/${PART}
+LODEV=`echo ${kpartx_out}|awk '{print $5}'`
+echo ${PART}
+echo ${LODEV}
+
+kpartx -u HD.IMG
+
+
+
+mount $PART /mnt/
+
+cp ./KERNEL.BIN /mnt/boot/Kernel
+cp scripts/grub.cfg /mnt/boot/grub2/
+
+md5sum /mnt/boot/Kernel
+
+mkdir -p /mnt/bin/
+cp ./bin/shell /mnt/bin/
+cp ./bin/hello /mnt/bin/
+md5sum /mnt/bin/*
+
+umount /mnt/
+
+
+kpartx -d ${LODEV}
+losetup -d ${LODEV}
