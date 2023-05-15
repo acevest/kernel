21116c82929056fbd4558f113997404fea2de8af
acevest <zhaoyanbai@126.com>
2023-05-15 23:23:45 +0800

    bochs boot from cdrom; improve mkiso.sh

--- mkiso.sh
+++ mkiso.sh
@@ -1,4 +1,14 @@
-CONTAINER_ID=78a
+
+if [ $# -ne 1 ]; then
+    docker ps -a
+    echo "input containerid "
+    read CONTAINER_ID
+else
+    CONTAINER_ID=$1
+fi
+
+echo "container id ${CONTAINER_ID}"
+
 docker exec -it $CONTAINER_ID /bin/bash -c "cd /root/workspace/kernel && ./scripts/mkiso.sh"
 
 
