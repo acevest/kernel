21d2c3a60cd6207a6d94b61694492c00a0aff043
acevest <zhaoyanbai@126.com>
2025-12-30 10:39:00 +0800

    multiboot2的头从multiboot.S挪到multiboot2_header.c里用C代码编写

---
+++ boot/multiboot2_header.c
@@ -0,0 +1,55 @@
+/*
+ * ------------------------------------------------------------------------
+ *   File Name: multiboot2_header.c
+ *      Author: Zhao Yanbai
+ *              2025-12-30 08:48:35 Tuesday CST
+ * Description: none
+ * ------------------------------------------------------------------------
+ */
+
+
+
+ #include <multiboot2.h>
+
+
+ #define ENABLE_FB 0
+
+ #define ALIGN8 __attribute__((aligned(8)))
+
+ typedef struct ALIGN8 multiboot2_elf_header {
+    ALIGN8 struct multiboot_header header;
+#if ENABLE_FB
+    ALIGN8 struct multiboot_header_tag_framebuffer fb;
+#endif
+    ALIGN8 struct multiboot_header_tag end;
+ } multiboot2_bin_header_t;
+
+
+
+ __attribute__((section(".multiboot2_header"), used))
+ const multiboot2_bin_header_t multiboot2_elf_header = {
+    .header = {
+        .magic = MULTIBOOT2_HEADER_MAGIC,
+        .architecture = MULTIBOOT_ARCHITECTURE_I386,
+        .header_length = sizeof(multiboot2_bin_header_t),
+        .checksum = -(MULTIBOOT2_HEADER_MAGIC + MULTIBOOT_ARCHITECTURE_I386 + sizeof(multiboot2_bin_header_t)),
+    },
+#if ENABLE_FB
+    .fb = {
+        .type = MULTIBOOT_HEADER_TAG_FRAMEBUFFER,
+        .flags = MULTIBOOT_HEADER_TAG_OPTIONAL,
+        .size = sizeof(struct multiboot_header_tag_framebuffer),
+        .width = 0,
+        .height = 0,
+        .depth = 0,
+    },
+#endif
+    .end = {
+        // tags are terminated by a tag of type ‘0’ and size ‘8’.
+        // MULTIBOOT_HEADER_TAG_END == 0
+        // sizeof(struct multiboot_header_tag) == 8
+        .type = MULTIBOOT_HEADER_TAG_END,
+        .flags = 0,
+        .size = sizeof(struct multiboot_header_tag),
+    },
+ };
